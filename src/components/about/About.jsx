5f38c4ec1557d74a2ae54faaeab6fa30f3245fab
bilaltahiraziz <bilal.aziz@live.com>
2022-05-30 16:33:59 -0500

    style components

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/Profile Pic 3.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FaUserGraduate } from 'react-icons/fa'
+import { VscFolderActive } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+    <h2>About Me</h2>
+
+    <div className="container about_container">
+
+      <div className="about_me">
+        <div className="about__me-image">
+          <img src={ME} alt="About me image"/>
+        </div>
+      </div>
+
+      <div className="about_content">
+        <div className="about__cards">
+
+          <article className='about__card'>
+            <FaAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small>General Assembly: Full-Immersive SEI<br /></small>
+            <small>Free Code Camp: Back End Development and API Certification<br /></small>
+          </article>
+
+          <article className='about__card'>
+            <FaUserGraduate className='about__icon'/>
+            <h5>Education</h5>
+            <small>General Assembly Bootcamp <br /></small>
+            <small>Southern Illinois University Edwardsville <br /></small>
+            <small>Ranken Technical College<br /></small>
+          </article>
+
+          <article className='about__card'>
+            <VscFolderActive className='about__icon'/>
+            <h5>Projects</h5>
+            <small>20+ Completed Projects</small>
+          </article>
+
+          <p><br />A functional department manager and avid member of the PCMasterRace community, with a daily dose of innovation. I enjoy the pursuit of optimal processes that we use every day, creatively finding faster and smoother solutions to our daily problems, everything from cable management to department synergy. Aiming to leverage my academic experience and proven knowledge of programming to help businesses and companies reach their maximum potential through innovation, technology, and SMART Goals.</p>
+
+          <p>Forever living by the quote, “You can't manage, what you don't measure” - Peter Drucker</p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+      </div>
+
+    </div>
+    </section>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,6 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
         <h1>Bilal Aziz</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA/>
@@ -18,7 +17,7 @@ const Header = () => {
           <img src={ME} alt="me" />
         </div>
 
-        <a href="#contact" className='scroll_down'>Scroll Down</a>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
