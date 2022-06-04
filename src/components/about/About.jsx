3a59438d9f1d4ee0a308d181bbb60a39d10f0741
bilaltahiraziz <bilal.aziz@live.com>
2022-06-03 23:52:19 -0500

    styling fixes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/Profile Pic 3.jpg'
+// import ME from '../../assets/Profile Pic 2.jpg'
+// import LAbilly from '../../assets/LA billy.jpg'
 import { FaAward } from 'react-icons/fa'
 import { FaUserGraduate } from 'react-icons/fa'
 import { VscFolderActive } from 'react-icons/vsc'
@@ -12,9 +13,10 @@ const About = () => {
 
     <div className="container about_container">
 
-      <div className="about_me">
+      <div className="about__me">
         <div className="about__me-image">
-          <img src={ME} alt="About me image"/>
+          {/* <img src={ME} alt="About me"/>
+          <img src={LAbilly} alt="About me"/> */}
         </div>
       </div>
 
@@ -25,7 +27,6 @@ const About = () => {
             <FaAward className='about__icon'/>
             <h4>Experience<br /><br /></h4>
             <h5> General Assembly:</h5> <small> Software Engineering Immersive Certification <br /></small>
-            <h5> Free Code Camp:</h5> <small> Back End Development and API Certification <br /></small>
           </article>
 
           <article className='about__card'>
@@ -41,16 +42,11 @@ const About = () => {
             <h5>Projects</h5>
             <small>20+ Completed Projects</small>
           </article>
-
-          <p><br />A functional department manager and avid member of the PCMasterRace community, with a daily dose of innovation. I enjoy the pursuit of optimal processes that we use every day, creatively finding faster and smoother solutions to our daily problems, everything from cable management to department synergy. Aiming to leverage my academic experience and proven knowledge of programming to help businesses and companies reach their maximum potential through innovation, technology, and SMART Goals.</p>
-
-          <p>Forever living by the quote, “You can't manage, what you don't measure” - Peter Drucker</p>
-
-          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
-
         </div>
       </div>
+      <p><br/>A functional department manager and avid member of the PCMasterRace community, with a daily dose of innovation. I enjoy the pursuit of optimal processes that we use every day, creatively finding faster and smoother solutions to our daily problems, everything from cable management to department synergy. Aiming to leverage my academic experience and proven knowledge of programming to help businesses and companies reach their maximum potential through innovation, technology, and SMART Goals. <br /><br />Forever living by the quote, “You can't manage, what you don't measure” - Peter Drucker<br/><br/></p>
 
+<a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
     </section>
   )
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -7,7 +7,6 @@ import { AiOutlineStar } from 'react-icons/ai'
 const Experience = () => {
   return (
     <section id='experience'>
-      <h5>Skills</h5>
       <h2>My Experience</h2>
 
       <div className='container experience__container'>
